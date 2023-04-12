dba6eff2c2318b5d9dac49b5f9bf6c8a6a747d84
Neil Smith <me+github@neilsmith.fyi>
2023-04-11 23:45:16 -0500

    remove useOnClickOutside

--- src/__tests__/useOnClickOutside.test.tsx
+++
@@ -1,7 +0,0 @@
-import useOnClickOutside from "../useOnClickOutside"
-
-describe("useOnClickOutside", () => {
-  it("fires the handler when outside the ref is clicked", () => {
-    expect(true).toBe(false)
-  })
-})
